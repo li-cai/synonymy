8027b336c5257fe642f8c9747cb646dce9ef86c3
Student <Student@pigpen41.istlabs.rit.edu>
2014-12-01 16:21:26 -0500

    Initial commit of project files

---
+++ Synonymy/Synonymy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Synonymy
+//
+//  Created by Student on 12/1/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Synonymy/Synonymy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Synonymy
+//
+//  Created by Student on 12/1/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
