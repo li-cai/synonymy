5be43176204be99a444e46422009b3e2a2257ab4
Cailin Li <cxl2467@rit.edu>
2014-12-01 21:00:56 -0500

    Update front page storyboard layout

---
+++ Synonymy/Synonymy/Sentence.h
@@ -0,0 +1,13 @@
+//
+//  Sentence.h
+//  Synonymy
+//
+//  Created by Student on 12/1/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Sentence : NSObject
+
+@end
---
+++ Synonymy/Synonymy/Sentence.m
@@ -0,0 +1,13 @@
+//
+//  Sentence.m
+//  Synonymy
+//
+//  Created by Student on 12/1/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import "Sentence.h"
+
+@implementation Sentence
+
+@end
