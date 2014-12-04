080672f1b090efd7fe7da4ada20b2bec8df424ac
Cailin Li <cxl2467@rit.edu>
2014-12-03 20:48:22 -0500

    Add shellscript for speedy git config c:

---
+++ gitconfig.sh
@@ -0,0 +1,5 @@
+ECHO "============ Git Config c: ============"
+git config --global user.name "Cailin Li"
+git config --global user.email "cxl2467@rit.edu"
+git config user.name
+git config user.email
