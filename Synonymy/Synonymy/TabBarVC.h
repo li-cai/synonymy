be12d42bc0104ade60178a2cddbe8bd7d1370e06
Cailin Li <cxl2467@rit.edu>
2014-12-12 17:27:11 -0500

    Add tab bar layout for navigation

---
+++ Synonymy/Synonymy/TabBarVC.h
@@ -0,0 +1,13 @@
+//
+//  TabBarVC.h
+//  Synonymy
+//
+//  Created by Student on 12/12/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarVC : UITabBarController
+
+@end
---
+++ Synonymy/Synonymy/TabBarVC.m
@@ -0,0 +1,43 @@
+//
+//  TabBarVC.m
+//  Synonymy
+//
+//  Created by Student on 12/12/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import "TabBarVC.h"
+
+@interface TabBarVC ()
+@end
+
+@implementation TabBarVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void) viewDidLayoutSubviews {
+    CGFloat tabBarHeight = 60.0;
+    CGRect frame = self.view.frame;
+    self.tabBar.frame = CGRectMake(0, frame.size.height - tabBarHeight, frame.size.width, tabBarHeight);
+    self.tabBar.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
