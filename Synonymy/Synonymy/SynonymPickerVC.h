a3bc1be3b09477e553b16354d6a5f571a704777b
Cailin Li <cxl2467@rit.edu>
2014-12-14 22:34:33 -0500

    Add UIPickerView in controller in storyboard and custom class

---
+++ Synonymy/Synonymy/SynonymPickerVC.h
@@ -0,0 +1,15 @@
+//
+//  SynonymPickerVC.h
+//  Synonymy
+//
+//  Created by Student on 12/14/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SynonymPickerVC : UIViewController<UIPickerViewDelegate, UIPickerViewDataSource>
+
+- (void) setSynonyms:(NSMutableArray *)synonyms;
+
+@end
---
+++ Synonymy/Synonymy/SynonymPickerVC.m
@@ -0,0 +1,57 @@
+//
+//  SynonymPickerVC.m
+//  Synonymy
+//
+//  Created by Student on 12/14/14.
+//  Copyright (c) 2014 Cailin Li. All rights reserved.
+//
+
+#import "SynonymPickerVC.h"
+
+@interface SynonymPickerVC ()
+@property (nonatomic) IBOutlet UIPickerView *pickerView;
+@property (nonatomic, strong) NSMutableArray *synonyms;
+@end
+
+@implementation SynonymPickerVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    _pickerView.delegate = self;
+    _pickerView.dataSource = self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) setSynonyms:(NSMutableArray *)synonyms {
+    _synonyms = synonyms;
+}
+
+- (int) numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return _synonyms.count;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return _synonyms[row];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
