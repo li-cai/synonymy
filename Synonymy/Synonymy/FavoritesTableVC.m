f67225602323dcfe4b021b18fe55c2836a5d9eca
Cailin Li <cxl2467@rit.edu>
2014-12-16 16:04:53 -0500

    Re-implement FILLER text to work with Favorite object

--- Synonymy/Synonymy/FavoritesTableVC.m
+++ Synonymy/Synonymy/FavoritesTableVC.m
@@ -39,7 +39,7 @@ NSString *FILLER = @"No favorites have been added.";
         NSAttributedString *tempstr = [[NSAttributedString alloc]initWithString:FILLER];
         Sentence *tempsent = [[Sentence alloc] initWithSentence:FILLER];
         
-        [temp addObject:[[Favorite alloc] initWithSentence:nil attrText:tempstr]];
+        [temp addObject:[[Favorite alloc] initWithSentence:tempsent attrText:tempstr]];
 
         _favorites = temp;
     }
@@ -105,7 +105,7 @@ NSString *FILLER = @"No favorites have been added.";
     Favorite *fav = _favorites[indexPath.row];
     Sentence *sent = fav.sentence;
     
-    if ([sent isEqual:FILLER]) {
+    if ([sent.fullsentence isEqual:FILLER]) {
         return NO;
     }
     
@@ -118,35 +118,6 @@ NSString *FILLER = @"No favorites have been added.";
         [self.tableView reloadData];
     }
 }
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
 
 /*
 // Override to support rearranging the table view.
